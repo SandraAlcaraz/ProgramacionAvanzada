1ecf4916e2b516d41ac3e26a9147a4842bca584b
Sandra G. Alcaraz Amezcua <sandra_alcaraz.a@outlook.com>
2018-04-02 16:12:00 -0500

    kata8

---
+++ katas/kata8/kata8.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int thrust;
+        int used = 0;
+        int BOOST;
+        int nextCheckpointX; // x position of the next check point
+        int nextCheckpointY; // y position of the next check point
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        scanf("%d%d%d%d%d%d", &x, &y, &nextCheckpointX, &nextCheckpointY, &nextCheckpointDist, &nextCheckpointAngle);
+        int opponentX;
+        int opponentY;
+        scanf("%d%d", &opponentX, &opponentY);
+
+        // Write an action using printf(). DON'T FORGET THE TRAILING \n
+        // To debug: fprintf(stderr, "Debug messages...\n");
+
+
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
+        // i.e.: "x y thrust"
+         
+        
+        if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90 ){
+            thrust = 0;
+        }
+        else{
+            thrust = 100;
+        }
+         if (nextCheckpointAngle == 0 && used != -1 && nextCheckpointDist >10000){
+            printf("%d %d BOOST\n", nextCheckpointX, nextCheckpointY);
+            used = -1;
+        }
+        else{
+             printf("%d %d %d\n", nextCheckpointX, nextCheckpointY,thrust);
+        }
+      
+    }
+
+    return 0;
+}
